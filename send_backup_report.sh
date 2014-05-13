72eaa283eb8ca2a51625bbb71f1bdafe40dbf14a
Diogo Kersting <diogovk@gmail.com>
2014-05-13 17:44:06 -0300

    adapt for use in more servers

--- send_backup_report.sh
+++ send_backup_report.sh
@@ -5,13 +5,17 @@ mymail(){
   sed 's/^/  /g' | mailx -s "$1" "$2"
 }
 
+
+BACKUP_LOG="$(echo /db2/db2[a-z][a-z][a-z]/backup.log)"
+[ -f "$BACKUP_LOG" ] || BACKUP_LOG="$(echo /usr/sap/[A-Z][A-Z][A-Z]/backup.log)"
+
 {
   grep /backup.sh /var/log/cron | tail -20
   echo '=================='
-  tail -15 /db2/db2smn/backup.log
+  tail -15 "$BACKUP_LOG"
   echo '=================='
-  ls -lad /backup/DB2SMN/SMN.0.db2smn.*
+  ls -lad /backup/DB2[A-Z][A-Z][A-Z]/[A-Z][A-Z][A-Z].0.db2[a-z][a-z][a-z].* /backup/SBO*/[bB]* 2>/dev/null
   echo '=================='
-  du -hs /backup/DB2SMN/SMN.0.db2smn.*
+  du -hs /backup/DB2[A-Z][A-Z][A-Z]/[A-Z][A-Z][A-Z].0.db2[a-z][a-z][a-z].* /backup/SBO*/[bB]* 2>/dev/null
 } | mymail "report backup $(hostname)" diogo.ti@malwee.com.br
 
